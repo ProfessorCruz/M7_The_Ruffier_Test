0018595a4f5152e531f208abe6914eebbdd8ae93
irsis22 <manteigakm@gmail.com>
2024-11-30 15:55:43 +0000

    Terminei a parte do 'inst'

--- inst.py
+++ inst.py
@@ -5,7 +5,15 @@ win_x , win_y = 200,100
 win_width, win_height = 1000, 600
 
 txt_hello = 'Welcome to the Health status detection program!'
-txt_instruction = 'This application allows you to use the Rufier test for...'
+txt_instruction = '''This application allows you to use the Rufier test for your health.
+The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.
+The subject lies in the supine position for 5 minutes and has their pulse rate measure for 15 seconds;
+then, within 45 seconds, the subject performs 30 squats.
+When the exercise end, the subeject lies down and their pulse is measured again for the fisrt 15 seconds
+and then for the last 15 seconds of the first minutes of the recovery period.
+Important! If you fell unwell during the test (dizziness,
+tinnitus, shortness of breath, etc.), stop the test and consult a physician.
+'''
 txt_next = 'Start'
 
 txt_index = 'Roufier Index: '
